ca929a2d292517c48b71af1aaa112e34b5aa9edf
Pankaj Pandey <52026014+PanPey@users.noreply.github.com>
2022-09-27 09:31:04 +0530

    Add files via upload

---
+++ backend/src/data.ts
@@ -0,0 +1,59 @@
+
+export const sample_foods: any[] = [
+  {
+    id:'1',
+    name: 'Biryani',
+    cookTime: '10-20',
+    price: 100,
+    imageUrl: 'assets/food-1.jpg',
+    tags: ['FastFood', 'Lunch', 'Dinner'],
+  },
+  {
+    id:'2',
+    name: 'Dhokla',
+    price: 100,
+    cookTime: '20-30',
+    imageUrl: 'assets/food-2.jpg',
+    tags: ['Breakfast', 'Lunch'],
+  },
+  {
+    id:'3',
+    name: 'Fish Curry',
+    price: 100,
+    cookTime: '10-15',   
+    imageUrl: 'assets/food-3.jpg',
+    tags: ['FastFood', 'Lunch'],
+  },
+  {
+    id:'4',
+    name: 'Aloo Paratha',
+    price: 100,
+    cookTime: '15-20',
+    imageUrl: 'assets/food-4.jpg',
+    tags: ['FastFood', 'Breakfast'],
+  },
+  {
+    id:'5',
+    name: 'Pizza',
+    price: 100,
+    cookTime: '40-50',
+    imageUrl: 'assets/food-5.jpg',
+    tags: ['Lunch', 'Fastfood'],
+  },
+  {
+    id:'6',
+    name: 'French Fries',
+    price: 9,
+    cookTime: '40-50',
+    imageUrl: 'assets/food-6.jpg',
+    tags: ['FastFood','Lunch'],
+  },
+]
+
+export const sample_tags:any[] = [
+  { name: 'All', count: 6 },
+  { name: 'FastFood', count: 4 },
+  { name: 'Lunch', count: 3 },
+  { name: 'Dinner', count: 2},
+  { name: 'Breakfast', count: 2}
+]
